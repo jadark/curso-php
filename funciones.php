1afd881d7269c1262465dbc3f8598879b4a2ab3e
Javier Castro <javiercastro.ik@gmail.com>
2017-02-12 00:20:52 -0500

    falta video scope

--- funciones.php
+++ funciones.php
@@ -1,6 +1,13 @@
 <?php
-function saludo(){
-  echo 'Holi bola';
+function saludo($nombre){
+  return "Saludo de $nombre";
 }
-saludo();
+function sumar($value1, $value2){
+  $resultado = $value1 + $value2;
+  return $resultado;
+}
+
+echo saludo('Carlos') . '<br/>';
+
+echo sumar(10, 30) . '<br/>';
 ?>
